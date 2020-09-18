49b74b74f34647f4194806841b0204514c4cf0a5
Alex <nexus.olivieri@gmail.com>
2020-09-18 15:52:04 +0200

    correct jquery connect ssl

--- area_riservata/home/admin/index.php
+++ area_riservata/home/admin/index.php
@@ -20,7 +20,7 @@
   <link href="assets/css/paper-dashboard.css?v=2.0.1" rel="stylesheet" />
   <!-- CSS Jus for demo purpose, don't include it in your project -->
   <link href="assets/css/mycss.css" rel="stylesheet" />
-  <script src="http://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
   <script src="page/assets/menu_left.js"></script>
 </head>
 
--- area_riservata/home/admin/page/clienti.php
+++ area_riservata/home/admin/page/clienti.php
@@ -20,7 +20,7 @@
   <link href="../assets/css/paper-dashboard.css?v=2.0.1" rel="stylesheet" />
   <link href="../assets/demo/demo.css" rel="stylesheet" />
   <link href="../assets/css/mycss.css" rel="stylesheet" />
-  <script src="http://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
   <script src="assets/menu_left.js"></script>
 </head>
 
